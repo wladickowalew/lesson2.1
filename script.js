17d392da1f709cc629d2026ac3f15705f0b1be69
Wlad Kowalew <wladickowalew@gmail.com>
2019-08-12 11:30:24 +0300

    add square and circle

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let canvas = document.getElementById('sandbox'),
-	context = canvas.getContext("2d");
+	context = canvas.getContext("2d"),
+	square, circle;
 
-context.fillRect(50, 25, 200, 250);
+square = new Path2D();
+square.moveTo(50, 50);
+square.lineTo(250,50);
+square.lineTo(250,250);
+square.lineTo(50,250);
+square.lineTo(50,50);
+context.stroke(square);
+
+circle = new Path2D();
+circle.arc(150, 150, 100, Math.PI/2, 3*Math.PI/2);
+context.fill(circle);
+
